ccaa63ede6ea12dde57333434b03c7180fd3da3d
Pinsius <lubonnt@gmail.com>
2017-02-11 19:19:23 +0100

    glm addition

--- PinGUI/CameraManager.cpp
+++ PinGUI/CameraManager.cpp
@@ -28,11 +28,31 @@ float* CameraManager::_staticCamera = nullptr;
 
 GLint CameraManager::_matrixLocation;
 
-void CameraManager::setCamera (float* cam){
+glm::mat4 CameraManager::_staticCameraMatrix;
 
-    _staticCamera = cam;
+int CameraManager::_screenWidth;
+
+int CameraManager::_screenHeight;
+
+glm::mat4 CameraManager::_orthoMatrix;
+
+void CameraManager::init(int screenWidth, int screenHeight){
+
+    _screenWidth = screenWidth;
+    _screenHeight = screenHeight;
+
+    PinGUI::Vector2<GUIPos> _position(screenWidth/2,screenHeight/2);
+
+    _orthoMatrix = glm::ortho(0.0f, (float)_screenWidth, 0.0f, (float)_screenHeight);
+
+    glm::vec3 translate(-_position.x + _screenWidth / 2, -_position.y + _screenHeight / 2, 0.0f);
+
+    _staticCameraMatrix = glm::translate(_orthoMatrix,translate);
 }
 
+glm::mat4 CameraManager::getCameraMatrix(){
+    return _staticCameraMatrix;
+}
 float* CameraManager::getCamera(){
     return _staticCamera;
 }
